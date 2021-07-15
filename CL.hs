0245450d3964f9525e04095f52e4086acdb4e2fd
joey <joeyvelez@knights.ucf.edu>
2021-07-15 16:57:33 -0400

    still need to add graph reps for CL, but done w/ string reps

--- CL.hs
+++ CL.hs
@@ -8,15 +8,14 @@ module CL where
 {- ================================= Syntax ================================= -}
 
 -- Combinatory logic (CL) terms
-data CL  = S | K | App CL CL deriving Show
+data Term  = S | K | App Term Term deriving Show
 
 
 {- ================================ Semantics =============================== -}
 
 -- Reduce (execute) a CL term
-reduce :: CL -> CL
+reduce :: Term -> Term
 reduce (App (App K x) y)         = x
 reduce (App (App (App S x) y) z) = (App (App x z) (App y z))
 reduce sk                        = sk
-
 
