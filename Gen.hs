1b55a7ae29c1f071bd396ad7fce09051f1f90e60
joey <joeyvelez@knights.ucf.edu>
2021-07-17 16:07:40 -0400

    refactored monad stack w/ state on bottom. need to both read and write the infinite list of fresh vars

--- Gen.hs
+++ Gen.hs
@@ -1,36 +1,39 @@
 {- Gen.hs
    ======
    Term generator for simply typed lambda calculus. With STLC2CL we can
-   also use the generator for combinatory logic. Implementation has
-   two stages.
-     (i) Type generation
-     (ii) Type-directed term generation -}
+   also use the generator for combinatory logic. -}
 
 import STLC
 
 import Control.Applicative
 import Control.Monad.Search
 import Control.Monad.Trans
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
 import Data.Monoid (Sum(..))
 
 
 {- ============================ Term Generator ============================== -}
 
--- Search monad lets us perform a weighted breadth first search
-genTerm :: Context -> Type -> Search (Sum Integer) Term
+-- Search + State monad stack.
+-- Search monad handles weighted nondeterminism
+-- State monad handles reading/writing of fresh variable names
+type SearchS c = SearchT c (State [Id])
+
+genTerm :: Context -> Type -> SearchS (Sum Integer) Term
 genTerm ctx ty = genUnit ctx ty
              <|> genBool ctx ty
              <|> genVar ctx ty
              <|> genProd ctx ty
 
 -- Generate unit
-genUnit :: Context -> Type -> Search (Sum Integer) Term
+genUnit :: Context -> Type -> SearchS (Sum Integer) Term
 genUnit _ (TyUnit) = do cost' (Sum 1)
                         return TmUnit
 genUnit _ _        = abandon
 
 -- Generate booleans
-genBool :: Context -> Type -> Search (Sum Integer) Term
+genBool :: Context -> Type -> SearchS (Sum Integer) Term
 genBool _ (TyBool) = do cost' (Sum 1)
                         return TmTrue
                  <|> do cost' (Sum 1)
@@ -38,15 +41,36 @@ genBool _ (TyBool) = do cost' (Sum 1)
 genBool _ _        = abandon
 
 -- Generate variables
-genVar :: Context -> Type -> Search (Sum Integer) Term
+genVar :: Context -> Type -> SearchS (Sum Integer) Term
 genVar []           ty' = abandon
 genVar ((x,ty):ctx) ty' | ty == ty' = do cost' (Sum 1)
                                          return (TmVar x)
                         | otherwise = genVar ctx ty'
 
-genProd :: Context -> Type -> Search (Sum Integer) Term
+genProd :: Context -> Type -> SearchS (Sum Integer) Term
 genProd ctx (TyProd ty1 ty2) = do cost' (Sum 1)
                                   TmProd
                                     <$> genTerm ctx ty1
                                     <*> genTerm ctx ty2
 genProd _ _                  = abandon
+
+
+-- Generate functions
+genFun :: Context -> Type -> SearchS (Sum Integer) Term
+genFun ctx (TyFun ty1 ty2) = do cost' (Sum 1)
+                                cost' (Sum (sizeTy ty1))
+                                x <- lift $ gets head
+                                lift $ modify tail
+                                TmFun x ty1
+                                  <$> genTerm ((x,ty1):ctx) ty2
+genFun _ _                 = abandon
+
+
+-- Helper function for generating functions
+-- NOTE: Size of terms includes size of type annotations when using genFun
+sizeTy :: Type -> Integer
+sizeTy (TyUnit)         = 1
+sizeTy (TyBool)         = 1
+sizeTy (TyProd ty1 ty2) = 1 + (sizeTy ty1) + (sizeTy ty2)
+sizeTy (TyFun ty1 ty2)  = 1 + (sizeTy ty1) + (sizeTy ty2)
+
