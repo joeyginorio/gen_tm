0968ca6f87d15a62798099cc92b2022920fd58ec
joey <joeyvelez@knights.ucf.edu>
2021-07-18 16:59:33 -0400

    added Aeson dependency to cabal, created Dataset.hs to host script for generating dataset

---
+++ src/Language/STLC/Dataset.hs
@@ -0,0 +1,9 @@
+{- Dataset.hs
+   ==========
+   Uses term generator to generate dataset in a JSON file, namely input-output
+   pairs where:
+      input  := unnormalized term
+      output := normalized   term -}
+
+import Language.STLC.Gen
+import Data.Aeson
