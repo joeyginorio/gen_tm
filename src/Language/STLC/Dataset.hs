c8e8d649039462eded21b5f136b9ce145fb0a72a
joey <joeyvelez@knights.ucf.edu>
2021-07-19 14:38:43 -0400

    working draft of script

--- src/Language/STLC/Dataset.hs
+++ src/Language/STLC/Dataset.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 {- Dataset.hs
    ==========
    Uses term generator to generate dataset in a JSON file, namely input-output
@@ -5,5 +10,28 @@
       input  := unnormalized term
       output := normalized   term -}
 
-import Language.STLC.Gen
+
+module Language.STLC.Dataset where
+
+import Language.STLC
+-- import Language.STLC.Gen
+
+import GHC.Generics
+-- import Data.Text.Lazy (Text)
+import Data.Text.Lazy.IO as I
+import Data.Aeson.Text (encodeToLazyText)
 import Data.Aeson
+
+deriving instance Generic Type
+deriving instance ToJSON Type
+
+deriving instance Generic Term
+deriving instance ToJSON Term
+
+data IOPair = IOPair {input :: Term, output :: Term}
+              deriving (Show, Generic, ToJSON)
+
+iop = IOPair TmUnit TmUnit
+
+main :: IO ()
+main = I.writeFile "src/Language/STLC/myfile.json" (encodeToLazyText [iop,iop])
