f23471c11c30ff3647e2fd99930926bb9d2f2b98
joey <joeyvelez@knights.ucf.edu>
2021-07-20 15:21:29 -0400

    S case on reduce wasnt reducing the applications it introduced

--- src/Language/CL.hs
+++ src/Language/CL.hs
@@ -17,5 +17,7 @@ data Term  = S | K | App Term Term
 -- | Reduce (execute) a CL term
 reduce :: Term -> Term
 reduce (App (App K x) y)         = reduce x
-reduce (App (App (App S x) y) z) = reduce (App (App x z) (App y z))
+reduce (App (App (App S x) y) z) = reduce (App xz yz)
+                                   where xz = reduce (App x z)
+                                         yz = reduce (App y z)
 reduce sk                        = sk
