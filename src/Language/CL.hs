2f96cb3445d38419844224a7438b2119f21c0711
joey <joeyvelez@knights.ucf.edu>
2021-07-29 18:35:09 -0400

    changed reduce to use writeto  rstore number of reducs

--- src/Language/CL.hs
+++ src/Language/CL.hs
@@ -8,6 +8,8 @@
 module Language.CL where
 
 import Data.Aeson.TH (defaultOptions, deriveJSON)
+import Control.Monad.Trans.Writer.Lazy
+import Data.Monoid
 
 {- ================================= Syntax ================================= -}
 
@@ -20,14 +22,28 @@ $(deriveJSON defaultOptions ''Term)
 
 {- ================================ Semantics =============================== -}
 
+-- | Writer term, writer monad tracks reduction steps
+type WTerm = Writer (Sum Integer) Term
+
 -- | Reduce (execute) a CL term
-reduce :: Term -> Term
-reduce (App (App K x) y)         = reduce x
-reduce (App (App (App S x) y) z) = reduce (App xz yz)
-                                   where xz = reduce (App x z)
-                                         yz = reduce (App y z)
-reduce (App x y)                 | x /= x' || y /= y' = reduce $ App x' y'
-                                 | otherwise          = App x' y'
-                                 where x' = reduce x
-                                       y' = reduce y
-reduce tm                        = tm
+reduce :: Term -> WTerm
+reduce (App (App K x) y)         = tell (Sum 1) >> reduce x
+reduce (App (App (App S x) y) z) = do tell $ Sum 1
+                                      xz <- reduce $ App x z
+                                      yz <- reduce $ App y z
+                                      reduce (App xz yz)
+reduce (App x y)                 = do tell $ Sum 1
+                                      x' <- reduce x
+                                      y' <- reduce y
+                                      if x /= x' || y /= y'
+                                        then reduce $ App x' y'
+                                        else return $ App x' y'
+reduce tm                        = return tm
+
+-- | Reduce, retaining writer output
+reduceW :: Term -> (Term, Sum Integer)
+reduceW = runWriter . reduce
+
+-- | Reduce, ignoring writer output
+reduce' :: Term -> Term
+reduce' = fst . reduceW
