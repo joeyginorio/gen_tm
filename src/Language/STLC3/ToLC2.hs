cc4248c1d7cd952701b33255fc7978f02a5498d8
Torsten Scholak <torsten.scholak@googlemail.com>
2021-09-24 16:27:24 -0400

    add stubs for nil, cons, and fold

--- src/Language/STLC3/ToLC2.hs
+++ src/Language/STLC3/ToLC2.hs
@@ -11,3 +11,6 @@ toLC2 (STLC3.TmVar x) = LC2.TmVar x
 toLC2 (STLC3.TmFun x _ty t) = LC2.TmFun x (toLC2 t)
 toLC2 (STLC3.TmIf s t u) = LC2.TmIf (toLC2 s) (toLC2 t) (toLC2 u)
 toLC2 (STLC3.TmApp s t) = LC2.TmApp (toLC2 s) (toLC2 t)
+toLC2 STLC3.TmNil = undefined
+toLC2 (STLC3.TmCons s t) = undefined
+toLC2 (STLC3.TmFold s t u) = undefined
